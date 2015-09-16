e24a2fd01555202c7993ff14421e1417fab3bec2
Synth Meat <synthmeat@gmail.com>
2015-09-16 21:44:55 +0200

    Revert "test revert"
    This reverts commit d435c59146a4716caaa68cadf554503da88982d6.

--- src/message.js
+++ src/message.js
@@ -1,125 +1,115 @@
-var util = require("util")
-    Protobuf = require("./libs/protobuf.js"),
-    Protocol = require("./libs/protocol.js"),
-    bytearray = require('bytearray');
+var util = require("util");
+var Protobuf = require("./libs/protobuf.js");
+var Protocol = require("./libs/protocol.js");
+var bytearray = require('bytearray');
 
-var Message = function(parent){
-    var self = this;
-    
-    self.parent = parent;
-    
-    self.MSG_FLAG_BYTES = 1;
-    self.MSG_ROUTE_CODE_BYTES = 2;
-    self.MSG_ID_MAX_BYTES = 5;
-    self.MSG_ROUTE_LEN_BYTES = 1;
-    self.MSG_ROUTE_CODE_MAX = 0xffff;
-    self.MSG_COMPRESS_ROUTE_MASK = 0x1;
-    self.MSG_TYPE_MASK = 0x7;
-    
-    self.TYPE_REQUEST = 0;
-    self.TYPE_NOTIFY = 1;
-    self.TYPE_RESPONSE = 2;
-    self.TYPE_PUSH = 3;
-    
-}
+var Message = function(parent) {
+  var self = this;
 
-var pt = Message.prototype;
+  self.parent = parent;
 
-pt.encode = function(id, route, msg){
-    var self = this,
-        msgStr = JSON.stringify(msg),
-        type = id ? self.TYPE_REQUEST : self.TYPE_NOTIFY,
-        byte = Protobuf.encode(route, msg) || Protocol.strencode(msgStr),
-        //byte = Protobuf.encode(route, msg),
-        rot = route;
-    
-    //console.log("buffer length: " + (5+byte.length) + " id: " + id);
-    var buffer = new Buffer(3+byte.length+rot.length);
-    buffer.fill(0x00);
-    //console.log("byte: " + util.inspect(byte.toString()));
-    //console.log("buffer-1 (empty): " + util.inspect(buffer));
-    if(!id)bytearray.writeByte(buffer, 0x00);
-    bytearray.writeByte(buffer, (type << 1) | ((typeof(rot) == "string") ? 0 : 1));
-    //console.log("buffer-2 (message flag)): " + util.inspect(buffer));
-    if(id){
-        do{
-            var tmp = id%128;
-            var next = Math.floor(id/128);
-            if(next!=0){
-                tmp = tmp +128;
-            }
-            //console.log("tmp: " + tmp);
-            bytearray.writeByte(buffer, tmp);
-            id = next;
-        } while(id!=0)
+  self.MSG_FLAG_BYTES = 1;
+  self.MSG_ROUTE_CODE_BYTES = 2;
+  self.MSG_ID_MAX_BYTES = 5;
+  self.MSG_ROUTE_LEN_BYTES = 1;
+  self.MSG_ROUTE_CODE_MAX = 0xffff;
+  self.MSG_COMPRESS_ROUTE_MASK = 0x1;
+  self.MSG_TYPE_MASK = 0x7;
+
+  self.TYPE_REQUEST = 0;
+  self.TYPE_NOTIFY = 1;
+  self.TYPE_RESPONSE = 2;
+  self.TYPE_PUSH = 3;
+};
+
+Message.prototype.encode = function(id, route, msg) {
+  var self = this;
+  var msgStr = JSON.stringify(msg);
+  var type = id ? self.TYPE_REQUEST : self.TYPE_NOTIFY;
+  var byte = Protobuf.encode(route, msg) || Protocol.strencode(msgStr);
+  var rot = route;
+
+  var buffer = new Buffer(3 + byte.length + rot.length);
+  buffer.fill(0x00);
+  if (!id) {
+    bytearray.writeByte(buffer, 0x00);
+  }
+  bytearray.writeByte(buffer, (type << 1) | ((typeof(rot) == "string") ? 0 : 1));
+  if (id) {
+    do {
+      var tmp = id%128;
+      var next = Math.floor(id/128);
+      if (next !== 0) {
+        tmp = tmp +128;
+      }
+      bytearray.writeByte(buffer, tmp);
+      id = next;
+    } while (id !== 0);
+  }
+
+  if (rot) {
+    if (typeof(rot) == "string") {
+      bytearray.writeByte(buffer, rot.length & 0xff);
+      bytearray.writeUTFBytes(buffer, rot);
     }
-    //console.log("buffer-3 (message id): " + util.inspect(buffer));
-    
-    if(rot){
-        //console.log("typeof(rot): " + typeof(rot));
-        if(typeof(rot) == "string"){
-            //console.log("it's a string! rot.length: " + rot.length);
-            //bytearray.writeByte(buffer, 0x00);
-            bytearray.writeByte(buffer, rot.length & 0xff);
-            bytearray.writeUTFBytes(buffer, rot);
-            //buffer.write(rot, 4);
-            
-            //Buffer.concat([buffer, Protocol.strencode(msgStr)]);
-            //console.log("check: " + buffer.toString());
-        } else {
-            //console.log("it's not a string!");
-            bytearray.writeByte(buffer, (rot>>8) & 0xff);
-            bytearray.writeByte(buffer, rot & 0xff);
-        }
+    else {
+      bytearray.writeByte(buffer, (rot >> 8) & 0xff);
+      bytearray.writeByte(buffer, rot & 0xff);
     }
-    
-    if(byte){
-        for(var b=0;b<byte.length;b++){
-            bytearray.writeByte(buffer, byte[b]);
-        }
-        //console.log("buffer-4: " + util.inspect(buffer));
-        return buffer;//bytearray.writeBytes(buffer, byte);
+  }
+
+  if (byte) {
+    for (var b = 0; b < byte.length; b++) {
+      bytearray.writeByte(buffer, byte[b]);
     }
     return buffer;
-}
+  }
 
-pt.decode = function(buffer){
-    //console.log("decoding: " + util.inspect(buffer));
-    var self = this,
-        flag = bytearray.readUnsignedByte(buffer, 0);
-        compressRoute = flag & self.MSG_COMPRESS_ROUTE_MASK,
-        type = (flag >> 1) & self.MSG_TYPE_MASK,
-        sliceFrom = 2;
-    
-    var id = 0;
-    //console.log("msg type: " + type);
-    if(type === self.TYPE_REQUEST || type === self.TYPE_RESPONSE){
-        var i = 0;
-        do{
-            var m = bytearray.readUnsignedByte(buffer);
-            id = id + ((m & 0x7f) * Math.pow(2, (7 * i)));
-            i++;
-        } while (m>=128)
+  return buffer;
+};
+
+Message.prototype.decode = function(buffer) {
+  var self = this;
+  var flag = bytearray.readUnsignedByte(buffer, 0);
+  var compressRoute = flag & self.MSG_COMPRESS_ROUTE_MASK;
+  var type = (flag >> 1) & self.MSG_TYPE_MASK;
+  var sliceFrom = 2;
+  var id = 0;
+  var m;
+  var route;
+
+  if (type === self.TYPE_REQUEST || type === self.TYPE_RESPONSE) {
+    var i = 0;
+    do {
+      m = bytearray.readUnsignedByte(buffer);
+      id = id + ((m & 0x7f) * Math.pow(2, (7 * i)));
+      i++;
+    } while (m >= 128);
+  }
+
+  if (type === self.TYPE_REQUEST || type === self.TYPE_NOTIFY || type === self.TYPE_PUSH) {
+
+    if (compressRoute) {
+      route = bytearray.readUnsignedShort(buffer);
     }
-    
-    if(type === self.TYPE_REQUEST || type === self.TYPE_NOTIFY || type === self.TYPE_PUSH){
-        var route;
-        //console.log("buffer: " + util.inspect(buffer));
-        if(compressRoute){
-            route = bytearray.readUnsignedShort(buffer);
-        } else {
-            var routeLen = bytearray.readUnsignedByte(buffer);
-            //console.log("routeLen: " + routeLen);
-            route = routeLen ? buffer.slice(2, routeLen+2).toString():"";
-            //console.log("route: " + route);
-            sliceFrom += routeLen;
-        }
-    } else if (type === self.TYPE_RESPONSE) {
-        route = self.parent.requests[id].route;
+    else {
+      var routeLen = bytearray.readUnsignedByte(buffer);
+      route = routeLen ? buffer.slice(2, routeLen + 2).toString() : '';
+      sliceFrom += routeLen;
     }
-    
-    var body = Protobuf.decode(route, buffer) || JSON.parse(Protocol.strdecode(buffer.slice(sliceFrom)));
-    return {id:id, type:type, route:route, body:body};
-}
+  }
+  else if (type === self.TYPE_RESPONSE) {
+    route = self.parent.requests[id].route;
+  }
+
+  var body = Protobuf.decode(route, buffer) ||
+             JSON.parse(Protocol.strdecode(buffer.slice(sliceFrom)));
+  return {
+    id: id,
+    type: type,
+    route: route,
+    body: body
+  };
+};
 
 module.exports = Message;
--- src/package.js
+++ src/package.js
@@ -1,81 +1,67 @@
-var util = require("util"),
-    bytearray = require('bytearray');
+var util = require("util");
+var bytearray = require('bytearray');
 
-var Package = function(){
-    var self = this;
-    
-    self.TYPE_HANDSHAKE = 1;
-    self.TYPE_HANDSHAKE_ACK = 2;
-    self.TYPE_HEARTBEAT = 3;
-    self.TYPE_DATA = 4;
-    self.TYPE_KICK = 5;
-}
+var Package = function() {
+  var self = this;
 
-var pt = Package.prototype;
+  self.TYPE_HANDSHAKE = 1;
+  self.TYPE_HANDSHAKE_ACK = 2;
+  self.TYPE_HEARTBEAT = 3;
+  self.TYPE_DATA = 4;
+  self.TYPE_KICK = 5;
+};
 
-pt.encode = function(type, body){
-    var self = this;
-    
-    var length = body ? body.length : 0;
-    //console.log("length: " + length);
-    var buffer = new Buffer(4+length);
-    var b = new Buffer(4);
-    buffer.fill(0x00);
-    bytearray.writeByte(buffer, type & 0xff);
-    //console.log("type set("+type+"): " + util.inspect(buffer));
-    bytearray.writeByte(buffer, (length>>16) & 0xff);
-    bytearray.writeByte(buffer, (length>>8) & 0xff);
-    bytearray.writeByte(buffer, (length>>8) & 0xff);
-    /*if(length<128)
-        bytearray.writeByte(buffer, length & 0xff);
-    else */b.writeUInt32BE(length,0);
-    //console.log("b: " + util.inspect(b));
-    //buffer[3]=b[0];
-    //if(body)buffer.write(body.toString(), 0, body.length);
-    //if(body)
-    //    console.log("body: " + body.toString());
-    for(var i=0;i<length;i++){
-        //console.log("i:"+i+"/"+length + " bl: " + buffer.length + " v: "+body[i]);
-        //bytearray.writeByte(buffer, body[i]);
-        buffer[(i+4)] = body[i];
+Package.prototype.encode = function(type, body) {
+  var self = this;
+
+  var length = body ? body.length : 0;
+  var buffer = new Buffer(4+length);
+  var b = new Buffer(4);
+
+  buffer.fill(0x00);
+  bytearray.writeByte(buffer, type & 0xff);
+  bytearray.writeByte(buffer, (length>>16) & 0xff);
+  bytearray.writeByte(buffer, (length>>8) & 0xff);
+  bytearray.writeByte(buffer, (length>>8) & 0xff);
+  b.writeUInt32BE(length,0);
+
+  for (var i = 0;i < length; i++) {
+    buffer[(i + 4)] = body[i];
+  }
+
+  buffer[3] = b[3];
+
+  if (length > 255) {
+    buffer[2] = b[2];
+  }
+
+  return buffer;
+};
+
+Package.prototype.decode = function(buffer) {
+  var self = this;
+  var body;
+  var type = bytearray.readUnsignedByte(buffer,0);
+  var len = (bytearray.readUnsignedByte(buffer) << 16 |
+             bytearray.readUnsignedByte(buffer) << 8 |
+             bytearray.readUnsignedByte(buffer)) >>> 0;
+
+  if (bytearray.getBytesAvailable(buffer) >= len) {
+    body = new Buffer(len);
+    if (len) {
+      for (var i = 4;i < len + 4; i++) {
+        bytearray.writeByte(body, buffer[i]);
+      }
+
     }
-    buffer[3]=b[3];
-    if(length>255){
-        buffer[2]=b[2];
-    }
-    //console.log("final: " + util.inspect(buffer));
-    //console.log("string: " + util.inspect(buffer.toString()));
-    
-    /*
-    var newBuffer = buffer.write(body.toString(),4)//Buffer.concat([buffer, body]);
-    console.log("head: " + util.inspect(buffer) + " body: " + body.toString() + " msg length: " + buffer.toString().length);
-    console.log("sending buffer: " + util.inspect(newBuffer.toString()));
-    return buffer.toString();*/
-    //console.log("head: " + util.inspect(buffer) + " body: " + body.toString() + " msg length: " + buffer.toString().length);
-    return buffer;
-}
+  }
 
-pt.decode = function(buffer){
-    var self = this;
-    
-    var type = bytearray.readUnsignedByte(buffer,0);
-    var len = (bytearray.readUnsignedByte(buffer)<<16 | bytearray.readUnsignedByte(buffer)<<8 | bytearray.readUnsignedByte(buffer))>>>0;
-    
-    var body;
-    //console.log("len: " + len + " type: " + type + " bytearray.getBytesAvailable(buffer): " + bytearray.getBytesAvailable(buffer));
-    if(bytearray.getBytesAvailable(buffer)>=len){
-        body = new Buffer(len);
-        if(len){
-            //body.write(buffer);//bytearray.readBytes(buffer, 0, len);
-            for(var i=4;i<len+4;i++){
-                bytearray.writeByte(body, buffer[i]);
-            }
-            
-        }
-    } else {
-        //console.log("insufficient length for current type: " + type);
-    }
-    return {type:type, body:body, length:len};
-}
+  return {
+    type: type,
+    body: body,
+    length: len
+  };
+};
 
 module.exports = Package;
+
