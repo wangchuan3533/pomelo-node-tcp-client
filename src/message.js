a438195b031bc320358d52907b7f23040b5968c1
Synth Meat <synthmeat@gmail.com>
2015-09-17 00:27:19 +0200

    shalala

--- src/message.js
+++ src/message.js
@@ -29,7 +29,7 @@ Message.prototype.encode = function(id, route, msg) {
   var byte = Protobuf.encode(route, msg) || Protocol.strencode(msgStr);
   var rot = route;
 
-  var buffer = new Buffer(3 + byte.length + rot.length);
+  var buffer = new Buffer(3 + byte.length * 2 + rot.length);
   buffer.fill(0x00);
   bytearray.writeByte(buffer, (type << 1) | ((typeof(rot) == "string") ? 0 : 1));
   if (id) {
